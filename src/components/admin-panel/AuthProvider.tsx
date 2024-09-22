b388116dfb3820b4ebbe7ac4ba65125368df98e9
Areesha Kainat <133228317+Areeesha-Kainat@users.noreply.github.com>
2024-09-22 13:30:52 +0500

    Add files via upload

---
+++ src/components/admin-panel/AuthProvider.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { SessionProvider } from 'next-auth/react';
+import { ReactNode } from 'react';
+
+interface PropTypes{
+    children: ReactNode
+  
+}
+
+const AuthProvider = ({children}:PropTypes) => {
+  return <SessionProvider>{children}</SessionProvider>;
+}
+
+export default AuthProvider;
